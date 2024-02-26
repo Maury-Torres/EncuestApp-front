efb4e882e04fe26b25fc371256fd8c9dd2622ae1
Mauricio Torres <mauriciotorres781@gmail.com>
2024-02-25 21:12:32 -0300

    modifique algunos archivos sin cambios significativos

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
+import { Register } from './pages/Register';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
       <h1>Proyecto Encuestas</h1>
+      <Register/>
     </>
   </React.StrictMode>,
 )
