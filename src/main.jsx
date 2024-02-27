606e6fbd7a2cbd7beeb3822fe2629f28347c72d0
Nicolas <nicopazmalizia@gmail.com>
2024-02-27 09:46:03 -0300

    Se realiza la pagina Home

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
+import './pages/Home.css'
+import Home from './pages/Home';
+import { Encuestas } from './pages/Encuestas';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <>
-      <h1>Proyecto Encuestas</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element = {<Home/>} />
+        <Route path='/encuestas' element = {<Encuestas/>} />
+        <Route path='*' element={<h1>Error</h1>}/>
+      </Routes>
+      </BrowserRouter>
+
     </>
   </React.StrictMode>,
 )
+
