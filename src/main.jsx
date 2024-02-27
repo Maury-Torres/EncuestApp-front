32800d93c9257e03ba5c81f6ec95e8a6518077ef
Mauricio Torres <mauriciotorres781@gmail.com>
2024-02-27 10:31:18 -0300

    agregando pagina de register a las rutas

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css';
 import { Register } from './pages/Register';
+import { NavbarTest } from './components/navbar/NavbarTest';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <h1>Proyecto Encuestas</h1>
-      <Register/>
-    </>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <NavbarTest/>
+        <Routes>
+          <Route path='/register' element={<Register />}/>
+        </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
