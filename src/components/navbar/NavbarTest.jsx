f5b24d7898417cec3d089091696bed88d891247b
Nicolas <nicopazmalizia@gmail.com>
2024-03-01 01:08:02 -0300

    Se modifican paginas home, banners y estilos de css

--- src/components/navbar/NavbarTest.jsx
+++ src/components/navbar/NavbarTest.jsx
@@ -1,30 +1,36 @@
 import React from 'react'
-import { Col, Container, Row, Nav, NavDropdown, Navbar } from 'react-bootstrap'
+import { Container, Nav, Navbar, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+import { container, button, button2, button3, btnHamburguesa, containerNav, title, navCollapse } from "./NavbarTest.module.css"
+// import estilos from "./NavbarTest.module.css";
+import logo from '/src/assets/logo.png'
 
 export const NavbarTest = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-              <Navbar.Brand href="#home">EncuestaAp</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Navbar.Text>
-                    <Link to='/'>Go to home</Link>
-                  </Navbar.Text>
-                  <Navbar.Text>
-                    <Link to='/register'>Go to Register</Link>
-                  </Navbar.Text>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </Col>
-      </Row>
-    </Container>
+    <Navbar expand="lg"  id={container} fixed='top'>
+      <Container>
+        <Navbar.Brand className='fw-bold text-white' id={title}>EncuestApp</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav text-white" id={btnHamburguesa}/>
+        <Navbar.Collapse id="basic-navbar-nav" className={navCollapse}>
+          <Nav className="gap-3" id={containerNav}>
+            <Button id={button} size="sm">
+              <Navbar.Text>
+                <Link to='/' className='fw-bold text-decoration-none'>Go to home</Link>
+              </Navbar.Text>
+            </Button >
+            <Button id={button2} size="sm">
+              <Navbar.Text>
+                <Link to='/register' className='fw-bold text-decoration-none'>Go to Register</Link>
+              </Navbar.Text>
+            </Button>
+            <Button id={button3} size="sm">
+              <Navbar.Text>
+                <Link to='/register' className='fw-bold text-decoration-none'>Admin</Link>
+              </Navbar.Text>
+            </Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
