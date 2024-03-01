223886df36757dd40b572772268a945be8bf0871
Mauricio Torres <mauriciotorres781@gmail.com>
2024-03-01 00:24:14 -0300

    agregando divs y secciones para poder hacerlo responsive al componente

--- src/components/home/HomeV1.jsx
+++ src/components/home/HomeV1.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Col, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
-import { container } from "./HomeV1.module.css"
+import { container, containerCol, secctionImg } from "./HomeV1.module.css"
 
 const HomeV1 = () => {
   return (
-    <Col>
-        <div className=" col-4" id={container}>
-          <h1>EncuestApp</h1>
-          <p>
-            Es una página web donde los usuarios pueden participar en diversas
-            encuestas y cuestionarios proporcionados por otros usuarios,
-            empresas o instituciones. Los usuarios pueden acceder a las
-            encuestas disponibles con su cuenta o de forma anónima, responder
-            preguntas según sus opiniones y experiencias, y enviar sus
-            respuestas de manera fácil y segura.
-          </p>
+    <Col id={containerCol} lg={6}>
+        <section className=" col-lg-10" id={container}>
+          <div>
+            <h1>EncuestApp</h1>
+            <p>
+              Es una página web donde los usuarios pueden participar en diversas
+              encuestas y cuestionarios proporcionados por otros usuarios,
+              empresas o instituciones. Los usuarios pueden acceder a las
+              encuestas disponibles con su cuenta o de forma anónima, responder
+              preguntas según sus opiniones y experiencias, y enviar sus
+              respuestas de manera fácil y segura.
+            </p>
+          </div>
           <div>
             <Link to="/encuestas">
               <Button variant="info" size="lg" className="rounded-5">
@@ -23,9 +25,10 @@ const HomeV1 = () => {
               </Button>
             </Link>
           </div>
-        </div>
-
-        <img src="./src/assets/encuestas2.png" alt="fondo" />
+        </section>
+        <section className='col-lg-4 mt-lg-0 mt-1' id={secctionImg}>
+          <img src="./src/assets/encuestas2.png" alt="fondo"/>
+        </section>
     </Col>
   );
 }
