d2e4777352cdfba79191ce4537f7f6fc6a8b5ab7
Daniel Cabral <danicabral15@outlook.com>
2024-03-04 18:39:10 -0300

    agregado selects order por fecha y filtrado por categoria en ABMPage

--- src/components/abm/ABM.jsx
+++
@@ -1,3 +0,0 @@
-export const ABM = () => {
-  return <div>ABM</div>;
-};
--- src/pages/ABMPage.jsx
+++ src/pages/ABMPage.jsx
@@ -2,19 +2,31 @@ import { useState } from "react";
 import { useFetch } from "../hooks/useFetch";
 import { ABMCard } from "../components/abm/card/ABMCard";
 import { ABMPagination } from "../components/abm/pagination/ABMPagination";
-import { Row } from "react-bootstrap";
+import { Row, Form } from "react-bootstrap";
 import styles from "./ABMPage.module.css";
+import { Select } from "../components/ui/select/Select";
 
 export const ABMPage = () => {
   const [page, setPage] = useState(1);
+  const [orderByDate, setOrderByDate] = useState("");
+  const [orderByCategory, setOrderByCategory] = useState("");
+
   const { data, error, hasError, isLoading } = useFetch(
-    `http://localhost:3000/api/encuestas?page=${page}`
+    `http://localhost:3000/api/encuestas?page=${page}&order=${orderByDate}&categoria=${orderByCategory}`
   );
 
   const [userName, setUserName] = useState("Admin"); //! Placeholder
 
   const handlePageChange = (pageNumber) => {
     setPage(pageNumber);
+  };
+
+  const handleOrderByDate = (date) => {
+    setOrderByDate(date);
+  };
+
+  const handleOrderByCategory = (category) => {
+    setOrderByCategory(category);
   };
 
   return (
@@ -25,6 +37,39 @@ export const ABMPage = () => {
           <button className="btn btn-primary p-3">Crear nueva encuesta</button>
           <button className="btn btn-primary p-3">Crear nueva categoria</button>
         </div>
+      </section>
+      <hr className="border-5" />
+      <section>
+        <article className="">
+          <p className="text-center">Ordenar por:</p>
+          <Select
+            name="order"
+            id="order"
+            value={orderByDate}
+            onChange={(e) => handleOrderByDate(e.target.value)}
+          >
+            <option disabled value="">
+              Fecha de creación
+            </option>
+            <option value="asc">Ascendente</option>
+            <option value="desc">Descendente</option>
+          </Select>
+          <p className="text-center mt-4">Filtrar por:</p>
+
+          <Select
+            name="category"
+            id="category"
+            value={orderByCategory}
+            onChange={(e) => handleOrderByCategory(e.target.value)}
+          >
+            <option disabled value="">
+              Categorias
+            </option>
+            <option value="Default">Por defecto</option>
+            <option value="Deportes">Deportes</option>
+            <option value="Animales">Animales</option>
+          </Select>
+        </article>
       </section>
       <hr className="border-5" />
       <section className={styles.list_abm}>
