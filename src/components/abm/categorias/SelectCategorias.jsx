c730ede52d25b3a16454941e27819dc4cc2494b8
Daniel Cabral <danicabral15@outlook.com>
2024-03-08 12:41:34 -0300

    Agregado componente SelectCategorias para manejar la selección de categorías

---
+++ src/components/abm/categorias/SelectCategorias.jsx
@@ -0,0 +1,34 @@
+import { useFetch } from "../../../hooks/useFetch";
+import { Select } from "../../ui/select/Select";
+import propTypes from "prop-types";
+export const SelectCategorias = ({
+  orderByCategory,
+  handleOrderByCategory,
+}) => {
+  const { data, isLoading } = useFetch("http://localhost:3000/api/categorias");
+  return (
+    <Select
+      name="categoria"
+      id="categoria"
+      value={orderByCategory}
+      onChange={(e) => handleOrderByCategory(e.target.value)}
+    >
+      <option disabled value="">
+        Categorias
+      </option>
+      <option value="Default">Por defecto</option>
+      {isLoading && <option value="">Loading...</option>}
+      {data &&
+        data.map((categoria) => (
+          <option key={categoria._id} value={categoria._id}>
+            {categoria.nombre}
+          </option>
+        ))}
+    </Select>
+  );
+};
+
+SelectCategorias.propTypes = {
+  orderByCategory: propTypes.string.isRequired,
+  handleOrderByCategory: propTypes.func.isRequired,
+};
