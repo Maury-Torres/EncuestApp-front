ce9070795574c257ae2153979322efdc63f75890
Daniel Cabral <danicabral15@outlook.com>
2024-03-15 15:52:01 -0300

    Agregado CategoriasProvider a Categorias routes

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import { EncuestasProvider } from "./context/EncuestaContext";
 import { Footer } from "./components/footer/Footer";
 import { Categorias } from "./components/categorias/Categorias";
 import { CategoriasForm } from "./components/categorias/form/CategoriasForm";
+import { CategoriasProvider } from "./context/CategoriaContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -33,13 +34,22 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           >
             <Route path="/abm" element={<ABMPage />} />
           </Route>
-          <Route path="/administrar-categoria" element={<CategoriasForm />} />
           <Route
-            path="/administrar-categoria/:id"
-            element={<CategoriasForm />}
-          />
+            element={
+              <CategoriasProvider>
+                <Outlet />
+              </CategoriasProvider>
+            }
+          >
+            <Route path="/administrar-categoria" element={<CategoriasForm />} />
+            <Route
+              path="/administrar-categoria/:id"
+              element={<CategoriasForm />}
+            />
 
-          <Route path="categorias" element={<Categorias />} />
+            <Route path="categorias" element={<Categorias />} />
+          </Route>
+
           <Route path="*" element={<h1>Error</h1>} />
         </Routes>
         <Footer />
