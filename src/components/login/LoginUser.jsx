ea861868b49a113e79b7219f7ba342fb2d3b3cb8
Your Name <you@example.com>
2024-03-16 01:17:39 -0300

    modificando login

--- src/components/login/LoginUser.jsx
+++ src/components/login/LoginUser.jsx
@@ -1,49 +1,92 @@
-import React, { useState, useRef } from 'react';
-import { Container, Col, Form, Button } from "react-bootstrap";
-import { container, input1, input2, submitBtn } from "../login/LoginUser1.module.css";
+import React, { useState } from 'react';
+import { Container, Col, Form, Button, InputGroup, } from "react-bootstrap";
+import { container, submitBtn, hiddenButton, inputField, inputField2 } from "../login/LoginUser1.module.css";
+import { passwordRegex } from "../../utils/passwordRegex.js";
+import { emailRegex } from "../../utils/emailRegex.js";
 import { alertcustom } from '../../utils/alertCustom';
+import { messages } from "../../utils/message.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { useForm } from 'react-hook-form';
+import Swal from "sweetalert2";
+import { Link } from 'react-router-dom';
 
-const BASE_URL= import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-const LoginUser = () => {
-  const formDataRef = useRef({
-    email: '',
-    password: '',
+export const LoginUser = () => {
+  const form = useForm();
+  const [errors] = useState({})
+  const { register} = form;
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    }
   });
 
-  const [errors, setErrors] = useState({});
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    formDataRef.current = {
-      ...formDataRef.current,
-      [name]: value
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (!emailRegex.test(e.email)) {
+        return alertcustom(
+          "ingrese un email valido"
+        )
+      }
+
+      if (!passwordRegex.test(e.password)) {
+        return alertcustom(
+          "La contraseña debe tener: una mayuscula, una minuscula, un numero, un caracter, min 8 caracteres",
+          "Error",
+          "warning"
+        );
+      }
+
+      const response = await fetch(`${BASE_URL}/api/signin`, {
+        method: 'POST',
+        credentials: "include",
+        headers: {
+          'Content-Type': 'application/json',
+          "acces-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify({
+          email: e.email,
+          password: e.password
+        }),
+      });
+
+      if (!response.status === 400) {
+        return alertcustom('Logueado', messages.congratulations, "success");
+      } else {
+          // alertcustom(messages.userSuccessful, messages.congratulations, "success", ()=> {});
+          Toast.fire({
+            icon: "success",
+            title: messages.userSuccessful
+          })
+      }
+      // Procesar la respuesta del backend según corresponda
+    } catch (error) {
+      console.log(error);
+      if (error.code == "ERR_NETWORK") {
+        alertcustom('Error de red', 'Error', 'warning')
+      }
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
-    const newErrors = {};
-
-    if (!formDataRef.current.email) {
-      newErrors.email = 'Ingrese su email';
-    }
-
-    if (!formDataRef.current.password) {
-      newErrors.password = 'Ingrese su contrseña';
-    }
-
-    setErrors(newErrors);
-
-    if (Object.keys(newErrors).length === 0) {
-      console.log(formDataRef);
-      alertcustom("logueado", "Felicidades", "successfull")
-    }
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
   };
 
   return (
-    <Col id={container} className="vh-50 w-100 d-flex"  >
+    <Col id={container} className="d-flex justify-content-center animate__animated animate__backInLeft"  >
       <Container>
         <div className="d-flex justify-content-center aling-items-center my-3 pb-3 border border-light border-0 border-bottom">
           <div className="ms-4 text-center">
@@ -51,23 +94,27 @@ const LoginUser = () => {
           </div>
         </div>
 
-
-        <Form onSubmit={handleSubmit}>
-
+        <Form noValidate onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label className='fw-bold text-black'>Email address</Form.Label>
             <Form.Control
-              id={input1}
+              id={inputField}
               type="email"
               placeholder="Enter email"
-              name='email'
-              defaultValue={formDataRef.current.email}
-              onChange={handleChange}
-              isValid={formDataRef.current.email && !errors.email}
-              isInvalid={!!errors.email}
+              tabIndex={0} 
+              className={errors.email?.message ? "is-invalid" : ""}
+              {...register("email", {
+                required: {
+                  value: true,
+                  message: "Ingrese un email",
+                },
+                pattern: {
+                  value: emailRegex,
+                  message: "Ingrese un email valido",
+                },
+              })}
             />
             <Form.Control.Feedback type='invalid'>
-              {errors.email}
             </Form.Control.Feedback>
             <Form.Control.Feedback type='valid'>
             </Form.Control.Feedback>
@@ -75,41 +122,56 @@ const LoginUser = () => {
 
           <Form.Group>
             <Form.Label className="fw-bold text-black">password</Form.Label>
-            <Form.Control
-              id={input2}
-              type="password"
-              placeholder="Password"
-              name='password'
-              defaultValue={formDataRef.current.password}
-              onChange={handleChange}
-              isValid={formDataRef.current.password && !errors.password}
-              isInvalid={!!errors.password}
-            />
-             <Form.Check
-              type="checkbox"
-              label ="Show password"
-            />
-            <Form.Control.Feedback type='invalid'>
-              {errors.password}
+            <InputGroup>
+              <Form.Control
+                id={inputField2}
+                type={passwordVisible ? "text" : "password"}
+                aria-describedby="passwordHelpBlock"
+                placeholder="Password"
+                name='password'
+                className={errors.password?.message ? "is-invalid" : ""}
+                {...register("password", {
+                  required: {
+                    value: true,
+                    message: "La contraseña es requerida",
+                  },
+                  pattern: {
+                    value: passwordRegex,
+                    message:
+                      "Su contraseña debe contener como minimo 8 caracteres, una letra mayúscula, una minúscula, un numero, un caracter especial",
+                  },
+                })}
+              />
+              <div className="input-group-append">
+                <button
+                  id={hiddenButton}
+                  type="button"
+                  className="toggle-password-visibility"
+                  onClick={togglePasswordVisibility}
+                >
+                  <FontAwesomeIcon icon={passwordVisible ? faEye : faEyeSlash} />
+                </button>
+              </div>
+              <Form.Control.Feedback type="invalid">
             </Form.Control.Feedback>
-            <Form.Control.Feedback type='valid'>
-            </Form.Control.Feedback>
+            </InputGroup>
           </Form.Group>
 
           <div className="fw-bold text-black">¿Todavía no te registraste?
-            <a className="m-1" href="/register">Suscríbete ahora</a>
+          <Link to='/register' className='fw-bold text-decoration-none'>Registrarse</Link>
           </div>
 
           <Button
             id={submitBtn}
-            className="my-3"
+            className='my-3'
             variant="primary"
-            type="submit">Login
+            type="submit">
+            Iniciar sesión
           </Button>
         </Form>
       </Container>
     </Col>
-  )
+  );
 };
 
 export default LoginUser;
