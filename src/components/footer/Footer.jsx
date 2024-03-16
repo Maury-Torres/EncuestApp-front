abd6212c72053f57d57d68860e72de83a8fbe186
Daniel Cabral <danicabral15@outlook.com>
2024-03-16 14:16:33 -0300

    Agregado logo al footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,13 @@
 import { CiFacebook, CiInstagram, CiTwitter } from "react-icons/ci";
 import { Container, Row, Col } from "react-bootstrap";
+import logo from "../../assets/logo-no-fondo.png";
 import styles from "./Footer.module.css";
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <Container>
-        <Row xs={1} sm={2} md={2} lg={2} className="align-items-center">
+        <Row xs={1} sm={3} md={3} lg={3} className="align-items-center">
           <Col className={`text-center py-4 text-white`}>
             <ul>
               <li>
@@ -26,6 +27,12 @@ export const Footer = () => {
               </li>
             </ul>
           </Col>
+          <Col className={`text-center py-4 text-white`}>
+            <a href="/">
+              <img src={logo} alt="Logo" className={styles.logo} />
+            </a>
+          </Col>
+
           <Col className={`text-center py-5 text-white`}>
             Derechos reservados &copy; {new Date().getFullYear()}
           </Col>
