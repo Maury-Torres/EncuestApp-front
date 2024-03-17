beeaae0525860cc8bd14314ec5da850dde623143
Mauricio Torres <mauriciotorres781@gmail.com>
2024-03-17 15:40:05 -0300

    incorporando logica a los botones de login y admin

--- src/components/navbar/NavbarTest.jsx
+++ src/components/navbar/NavbarTest.jsx
@@ -6,12 +6,37 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import estilos from "./NavbarTest.module.css";
 import logo from '/src/assets/logo.png'
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { useAuth } from '../../context/AuthContext';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+
 
 export const NavbarTest = () => {
 
-  const signout = () => {
-    
-  }
+  const { user, isAuth } = useAuth();
+  console.log(user, isAuth);
+
+  const signout = async () => {
+    try {
+      // Realizar una solicitud al backend para cerrar sesión
+      const response = await fetch(`${BASE_URL}/api/signout` , {
+        method: 'POST',
+        credentials: 'same-origin', // Asegura que las cookies se incluyan en la solicitud
+      });
+      
+      if (response.ok) {
+        // Limpiar la cookie de token en el cliente
+        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        // Redireccionar a la página de inicio de sesión u otra página
+        window.location.href = '/login'; // Cambia '/login' a la ruta de tu página de inicio de sesión
+      } else {
+        throw new Error('Error al cerrar sesión');
+      }
+    } catch (error) {
+      console.error('Error al cerrar sesión:', error);
+    }
+  };
+
 
   return (
     <Navbar expand="lg"  id={container} fixed='top'>
@@ -25,17 +50,27 @@ export const NavbarTest = () => {
                 <Link to='/' className='fw-bold text-decoration-none'>Go to home</Link>
               </Navbar.Text>
             </Button >
-            <Button id={button2} size="sm">
-              <Navbar.Text>
-                <Link to='/login' className='fw-bold text-decoration-none'>Login</Link>
-              </Navbar.Text>
-            </Button>
-            <Button id={button3} size="sm">
-              <Navbar.Text>
-                <Link to='/admin' className='fw-bold text-decoration-none'>Admin</Link>
-              </Navbar.Text>
-            </Button>
-            <Button id={button3} size="sm">
+
+            { isAuth == true ? ""
+            : <>
+                <Button id={button2} size="sm">
+                  <Navbar.Text>
+                    <Link to='/login' className='fw-bold text-decoration-none'>Login</Link>
+                  </Navbar.Text>
+                </Button>
+              </>
+            }
+            { isAuth && user.roles[0].nombre == 'admin' 
+            ? 
+                <Button id={button3} size="sm">
+                  <Navbar.Text>
+                    <Link to='/admin' className='fw-bold text-decoration-none'>Admin</Link>
+                  </Navbar.Text>
+                </Button>
+            : ''
+            }
+
+            <Button onClick={signout} id={button3} size="sm">
               <Navbar.Text>
                 <FontAwesomeIcon icon={faRightFromBracket} />
               </Navbar.Text>
