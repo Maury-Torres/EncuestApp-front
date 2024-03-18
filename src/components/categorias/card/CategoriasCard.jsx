29906bd52b1d142e3b17a7c9888af29118b0fafc
Daniel Cabral <danicabral15@outlook.com>
2024-03-18 12:31:35 -0300

    Agregado condicional roles en CategoriasCard

--- src/components/categorias/card/CategoriasCard.jsx
+++ src/components/categorias/card/CategoriasCard.jsx
@@ -1,48 +1,57 @@
 import propTypes from "prop-types";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { Card, Button } from "react-bootstrap";
 import styles from "./CategoriasCard.module.css";
 import { FaTrashAlt, FaPencilAlt } from "react-icons/fa";
+import { useAuth } from "../../../context/AuthContext";
 
 export const CategoriasCard = ({ categoria, handleOnBorrarCategoria }) => {
   const navigate = useNavigate();
+  const { user } = useAuth();
+  console.log(user);
 
   return (
     <Card className={`bg-dark text position-relative ${styles.card}`}>
-      <div className={styles.imageWrapper}>
-        <Card.Img
-          src={categoria.imagen}
-          alt={`Imagen de la categoria ${categoria.nombre}`}
-          className={`${styles.cardImage}`}
-        />
-        <div className={styles.filter} />
-      </div>
-      <div
-        className={`${styles.textOverlay} d-flex justify-content-center align-items-center`}
-      >
-        <Card.Title className="text-white text-center mt-3 fs-3 fs-sm-2 fs-md-1">
-          {categoria.nombre}
-        </Card.Title>
-        {/*  <Card.Text className="text-white fs-6 fs-sm-5 fs-md-4">
+      <Link to={`/encuestas/categoria/${categoria._id}`}>
+        <div className={styles.imageWrapper}>
+          <Card.Img
+            src={categoria.imagen}
+            alt={`Imagen de la categoria ${categoria.nombre}`}
+            className={`${styles.cardImage}`}
+          />
+          <div className={styles.filter} />
+        </div>
+        <div
+          className={`${styles.textOverlay} d-flex justify-content-center align-items-center`}
+        >
+          <Card.Title className="text-white text-center mt-3 fs-3 fs-sm-2 fs-md-1">
+            {categoria.nombre}
+          </Card.Title>
+          {/*  <Card.Text className="text-white fs-6 fs-sm-5 fs-md-4">
                         {el.descripcion}
                       </Card.Text> */}
-      </div>
-      <Card.Footer className={`${styles.adminFooter}`}>
-        <div className="d-flex w-100 justify-content-evenly">
-          <Button
-            variant="success"
-            onClick={() => navigate(`/administrar-categoria/${categoria._id}`)}
-          >
-            Editar <FaPencilAlt />
-          </Button>
-          <Button
-            variant="danger"
-            onClick={() => handleOnBorrarCategoria(categoria._id)}
-          >
-            Eliminar <FaTrashAlt />
-          </Button>
         </div>
-      </Card.Footer>
+      </Link>
+      {user && user?.roles.includes("Admin") && (
+        <Card.Footer className={`${styles.adminFooter}`}>
+          <div className="d-flex w-100 justify-content-evenly">
+            <Button
+              variant="success"
+              onClick={() =>
+                navigate(`/administrar-categoria/${categoria._id}`)
+              }
+            >
+              Editar <FaPencilAlt />
+            </Button>
+            <Button
+              variant="danger"
+              onClick={() => handleOnBorrarCategoria(categoria._id)}
+            >
+              Eliminar <FaTrashAlt />
+            </Button>
+          </div>
+        </Card.Footer>
+      )}
     </Card>
   );
 };
