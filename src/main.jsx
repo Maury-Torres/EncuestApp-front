fe10b825644fbd48eeee8478e1845eb6322f769d
Daniel Cabral <danicabral15@outlook.com>
2024-03-18 13:27:46 -0300

    Agregado AuthProvider a EncuestApp

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "animate.css";
 import { EncuestApp } from "./EncuestApp";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <EncuestApp />
+    <AuthProvider>
+      <EncuestApp />
+    </AuthProvider>
   </React.StrictMode>
 );
