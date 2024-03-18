6d6bf3891d4b5dbd215fe606d53a1fa5b7557b73
Daniel Cabral <danicabral15@outlook.com>
2024-03-18 13:27:32 -0300

    Refactorizado routes y implementado ProtectedRoute

--- src/EncuestApp.jsx
+++ src/EncuestApp.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import {
   ABMPage,
-  AuthProvider,
   Categorias,
   CategoriasForm,
   CategoriasProvider,
@@ -17,65 +16,83 @@ import {
 } from "./pages/index.js";
 import { ResponderEncuesta } from "./components/encuestas/responderEncuesta/ResponderEncuesta.jsx";
 import { VerResultados } from "./components/encuestas/verResultados/VerResultados.jsx";
+import { ProtectedRoute } from "./components/ProtectedRoute.jsx";
+import { useAuth } from "./context/AuthContext.jsx";
 
 export const EncuestApp = () => {
+  const { user, isAuth } = useAuth();
+  console.log(user);
   return (
     <div
       style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
     >
       <BrowserRouter>
-        <AuthProvider>
-          <NavbarTest />
-          <Routes>
-            <Route path="/" element={<HomeV1 />} />
+        <NavbarTest />
+        <Routes>
+          <Route path="/" element={<HomeV1 />} />
+          <Route
+            element={<ProtectedRoute isAllowed={!isAuth} redirectTo="/" />}
+          >
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
-            <Route
-              element={
+          </Route>
+
+          <Route
+            element={
+              <ProtectedRoute isAllowed={isAuth} redirectTo="/error">
                 <EncuestasProvider>
                   <Outlet />
                 </EncuestasProvider>
+              </ProtectedRoute>
+            }
+          >
+            <Route
+              element={
+                <ProtectedRoute
+                  isAllowed={isAuth}
+                  redirectTo="/error"
+                  /* allowedRoles={["Admin"]} */
+                  userRole={user && user.roles[0]}
+                />
               }
             >
               <Route path="/abm" element={<ABMPage />} />
-              <Route path="/encuestas/categoria/:id" element={<Encuestas />} />
-              <Route path="/administrar-encuesta" element={<EncuestasForm />} />
-              <Route
-                path="/administrar-encuesta/:id"
-                element={<EncuestasForm />}
-              />
-              <Route
-                path="/responder-encuesta/:id"
-                element={<ResponderEncuesta />}
-              />
-              <Route
-                path="/ver-resultados/:encuestarealizadaid"
-                element={<VerResultados />}
-              />
             </Route>
-
-            <Route path="*" element={<Errors />} />
-
+            <Route path="/encuestas/categoria/:id" element={<Encuestas />} />
+            <Route path="/administrar-encuesta" element={<EncuestasForm />} />
             <Route
-              element={
+              path="/administrar-encuesta/:id"
+              element={<EncuestasForm />}
+            />
+            <Route
+              path="/responder-encuesta/:id"
+              element={<ResponderEncuesta />}
+            />
+            <Route
+              path="/ver-resultados/:encuestarealizadaid"
+              element={<VerResultados />}
+            />
+          </Route>
+          <Route
+            element={
+              <ProtectedRoute isAllowed={isAuth}>
                 <CategoriasProvider>
                   <Outlet />
                 </CategoriasProvider>
-              }
-            >
-              <Route
-                path="/administrar-categoria"
-                element={<CategoriasForm />}
-              />
-              <Route
-                path="/administrar-categoria/:id"
-                element={<CategoriasForm />}
-              />
-              <Route path="/categorias" element={<Categorias />} />
-            </Route>
-          </Routes>
-          <Footer />
-        </AuthProvider>
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/administrar-categoria" element={<CategoriasForm />} />
+            <Route
+              path="/administrar-categoria/:id"
+              element={<CategoriasForm />}
+            />
+            <Route path="/categorias" element={<Categorias />} />
+          </Route>
+
+          <Route path="*" element={<Errors />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
