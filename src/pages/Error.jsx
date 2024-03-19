67ee5708058584de510fc39fc6e367426a58a198
Mauricio Torres <mauriciotorres781@gmail.com>
2024-03-19 02:58:22 -0300

    corrigiendo errores en la importacion de pagiga Error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Row } from 'react-bootstrap'
-import Errors from '../components/error/errors'
+import Errors from '../components/error/Errors'
 
 const Error = () => {
   return (
